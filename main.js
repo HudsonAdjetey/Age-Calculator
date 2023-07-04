bd7bf3ff044d0ebc44b0f0651139ffcd4bc256c6
HudsonAdjetey <adjetey545@gmail.com>
2023-07-04 05:00:40 -0700

    updated

--- main.js
+++ main.js
@@ -1,99 +1,140 @@
-const dsYears = document.querySelector('.dsYears span');
-const dsMonth = document.querySelector('.dsMonths span');
-const dsDay = document.querySelector('.dsDays span');
+// The output elements
+const outPutYear = document.querySelector('.outYear');
+const outPutMonth = document.querySelector('.outMonth');
+const outPutDay = document.querySelector('.outDay');
+// The output elements
 
-const btn = document.querySelector('.btn')
+// Get the input values
+let yearInput = document.getElementById('year');
+let monthInput = document.getElementById('month');
+let dayInput = document.getElementById('day');
+
+let isValid = false;
+
+// Get error Messages
+const dayError = document.querySelector('.error-day');
+const monthError = document.querySelector('.error-month');
+const yearError = document.querySelector('.error-year');
 
 
-btn.addEventListener('click', function() {
-    handleDate()
-})
+// Get the form container
+
+const formContainer = document.querySelector('.form-container');
+
+formContainer.addEventListener('keypress', (e) => {
+    if(e.key === 'Enter') {
+        handleCalculation(dayInput.value, monthInput.value, yearInput.value)
+    }
+} )
+
+// Get the btn for submission
+const submitBtn = document.querySelector('.btn');
+
+// Add eventlistener to the button
+submitBtn.addEventListener('click', () => {
+    let yearInput = document.getElementById('year');
+    let monthInput = document.getElementById('month');
+    let dayInput = document.getElementById('day');
+
+    handleCalculation(dayInput.value, monthInput.value, yearInput.value)
+} )
+
+// Add listeners to the inputs and also checking for errors
 
 
+// A function to handle the caclulations
 
+function handleCalculation(day, month, year) {
+    const yearInput = document.getElementById('year');
+    const monthInput = document.getElementById('month');
+    const dayInput = document.getElementById('day');
 
-function handleDate() {
-    let years = document.getElementById('year').value;
-    let months = document.getElementById('month').value;
-    let days = document.getElementById('day').value;
-    
     createAlert()
+    isValidError()
 
 
-    if(years == '' || months == '' || days == '') {
-        return  
+    if(yearInput.value == '' || monthInput.value == '' || dayInput.value == '') {
+        return
     }
 
-    
-    // The mathematics behind it
-    
-    
-    let date = new Date();
-
-    let calcDay = date.getDate();
-    let calcMonth = date.getMonth();
-    let calcYear = date.getFullYear();
-
-    let totalMonths = [31,28,31,30,31,31,30,31,30,31] 
-
-
-    if(days > calcDay) {
-        calcDay += totalMonths[calcMonth -1];
-        calcMonth = calcMonth -  1;
+    if(!isValidDate(day, month, year)) {
+        return
     }
+    // calculate the age
+    const currentDate = new Date();
+    const birthDate = new Date(year, month - 1, day);
 
-    if(months > calcMonth) {
-        calcMonth = calcMonth + 12
-        calcYear = calcYear -  1
+    if(birthDate.getFullYear() > currentDate.getFullYear()) {
+        return
     }
+    // calculate the difference in milliseconds
+    const diffTime = currentDate.getTime() - birthDate.getTime();
 
-    const daysRemaining = parseInt(calcDay - days)
-    const monthDiff = parseInt(calcMonth - months)
-    const yearDiff = parseInt(calcYear - years)
+    // Number of year,month and day in milliseconds
+    const millisecondsInYear = 1000 * 60 * 60 * 24 * 365.25;
+    const millisecondsInMonth = millisecondsInYear / 12;
+    const millisecondsInDay = 1000 * 60 * 60 * 24;
+
+    // Calculate the number of years, months and days
+    const years = Math.floor(diffTime / millisecondsInYear)
+    const months = Math.floor((diffTime % millisecondsInYear) / millisecondsInMonth )
+    const days = Math.floor((diffTime % millisecondsInMonth)/ millisecondsInDay)
 
 
-    let countYear = 0
-    let countMonth = 0
-    let countDay = 0
 
+    countYear = 0;
+    countMonth = 0
+    countDay = 0
+
+// Updating the DOM
+        // Year aninmate
     let yearAnimate = setInterval(()=> {
-        countYear++;
-        dsYears.innerHTML = `${countYear}`;
-        if(countYear === yearDiff) {
-            clearInterval(yearAnimate)
-        }
-    }, 200)
+    countYear++;
+    outPutYear.innerHTML = `${countYear}`;
+    if(countYear === years) {
+        clearInterval(yearAnimate)
+    }
+}, 200)
 
-    let monthAnimate = setInterval(()=> {
-        countMonth++;
-        dsMonth.innerHTML = `${countMonth}`;
-        if(countMonth === monthDiff) {
-            clearInterval(monthAnimate)
-        }
-    }, 200)
+    // Month animate
+let monthAnimate = setInterval(()=> {
+    countMonth++;
+    outPutMonth.innerHTML = `${countMonth}`;
+    if(countMonth === months) {
+        clearInterval(monthAnimate)
+    }
+}, 200)
 
+    // Day animate
+let dayAnimate = setInterval(()=> {
+    countDay++;
+    outPutDay.innerHTML = `${countDay}`;
+    if(countDay === days) {
+        clearInterval(dayAnimate)
+    }
+}, 200)
 
-    let dayAnimate = setInterval(()=> {
-        countDay++;
-        dsDay.innerHTML = `${countDay}`;
-        if(countDay === daysRemaining) {
-            clearInterval(dayAnimate)
-        }
-    }, 200)
-    
-    
-    
-    
-
-}    
+}
 
 
 
+// A function to set date, month and year valid
+function isValidDate(day, month, year) {
+    const date = new Date(year, month -1, day);
+
+    return (
+        date.getDate() === Number(day) && date.getMonth() === Number(month) - 1 && date.getFullYear() === Number(year)  
+        
+    )
+}
 
 
+
+
+// The function throws an alert when the input field is empty
 function createAlert() {
     let allInput = document.querySelectorAll('.inputs');
-    const setValue = document.querySelectorAll('.date span');
+    const setValue = document.querySelectorAll('.date .required');
     const labelField = document.querySelectorAll('label')
 
 
@@ -110,6 +151,38 @@ function createAlert() {
     }
 }
 
+// A function that checks the date errors
 
+function isValidError() {
+    const dayError = document.querySelector('.date .error-day');
+    const monthError = document.querySelector('.error-month');
+    const yearError = document.querySelector('.error-year');
 
+    // get all the inputs values
+    const yearInput = document.getElementById('year');
+    const monthInput = document.getElementById('month');
+    const dayInput = document.getElementById('day');
 
+    const newDate = new Date();
+
+    // A condition to check validity of the dates
+    if(yearInput.value > newDate.getFullYear()) {
+        yearError.textContent = 'Must be a valid date'
+    }else {
+        yearError.textContent = ''
+    }
+
+    if(monthInput.value > 12) {
+        monthError.textContent = 'Must be a valid date'
+    }
+    else {
+        monthError.textContent = ''
+    }
+    if(dayInput.value > 31) {
+        dayError.textContent = 'Must be a valid date'
+    }
+    else {
+        dayError.textContent = ''
+    }
+
+} 
